05f87daa300764b60402e889c98dd6205ab9668b
Maikol leon <maikolleon72@gmail.com>
2021-06-20 19:59:53 -0400

    primer commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c779eb8055d89244c57a0a80635dd098'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f14f81d5-8b28-4060-bbf4-d922ac57c73f'
+};
